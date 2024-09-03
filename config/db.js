632af290e67b3a59b1cb62af32ad08faf45f04c8
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-09-03 17:04:40 -0500

    Added database connection

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+const db = config.get('mongoURI');
+
+const connectDatabase = async () =>{
+    try{
+        await mongoose.connect(db, {
+            useUnifiedTopology: true
+        });
+        console.log('Connnected to MongoDB');
+
+    }catch(error){
+        console.error(error.message);
+    }
+};
+
+export default connectDatabase;
