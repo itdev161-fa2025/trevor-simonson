c720b9452ad51c2145b0b8aa90fd748aa4237486
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-09-03 16:35:36 -0500

    project setup

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) =>
 );
 
 // Connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+app.listen(3000, () => console.log('Express server running on port 3000')); 
